0a2d7061ed34dd2a3b9305a3b4ac2064f43181f6
beelzekamibub <55458991+beelzekamibub@users.noreply.github.com>
2023-12-14 23:01:58 +0530

    Update and rename quicksort to quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -1,6 +1,3 @@
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         QuickSort<Integer> obj=new QuickSort<>();
