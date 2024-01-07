d14b5efa88736e1dd9cda80bc053867b61acd1f8
sharmachait <55458991+sharmachait@users.noreply.github.com>
2024-01-07 23:49:07 +0530

    Update ArithmeticSlices2.java

--- ArithmeticSlices2.java
+++ ArithmeticSlices2.java
@@ -1,21 +1,33 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        int n = nums.length;
-        int result = 0;
-        Map<Long, Integer>[] mp = new HashMap[n];
-
-        for (int i = 0; i < n; i++) {
-            mp[i] = new HashMap<>();
-            for (int j = 0; j < i; j++) {
-                long diff = (long) nums[i] - nums[j];
-                int count_at_j = mp[j].getOrDefault(diff, 0);
-
-                result += count_at_j;
-                
-                // Increment the count at index i
-                mp[i].put(diff, mp[i].getOrDefault(diff, 0) + count_at_j + 1);
+        Map<Long,Integer> g[]=new Map[nums.length];
+        for(int i=0;i<nums.length;i++){
+            g[i]=new HashMap<>();
+        }
+        for(int curr=0;curr<nums.length;curr++){
+            for(int prev=0;prev<curr;prev++){
+                long diff=(long)nums[curr]-(long)nums[prev];
+                int res=0;
+                if(g[prev].containsKey(diff)){
+                    res=1+g[prev].get(diff);
+                }
+                else{
+                    res=1;
+                }
+                int already_counted_sequences=g[curr].getOrDefault(diff,0);
+                res=res+already_counted_sequences;
+                g[curr].put(diff,res);
             }
         }
-        return result;
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            for(long j:g[i].keySet()){
+                ans+=g[i].get(j);
+            }
+        }
+        int n=nums.length;
+        int ceil=(int)Math.ceil(n*(n-1)/2);
+        ans-=ceil;
+        return ans;
     }
 }
